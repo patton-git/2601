7145828c1f5cd8a9fa4bed8cca47e671f168bb86
patton-git <63215900+patton-git@users.noreply.github.com>
2026-01-30 16:10:58 +0900

    Fix syntax error in SSH key generation script

--- 2.Ansible/6.ansible_ssh_key2.sh
+++ 2.Ansible/6.ansible_ssh_key2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [ ! -f ~/.ssh/ansible_key ]; then ssh-keygen -t rsa -b 4096 -f ~/.ssh/ansible_key -N "" fi
+if [ ! -f ~/.ssh/ansible_key ]; then ssh-keygen -t rsa -b 4096 -f ~/.ssh/ansible_key -N "" 
+fi
 
 SERVERS=( "10.10.15.10" "10.10.15.11" "10.10.15.12" "10.10.15.21" "10.10.15.22" "10.10.15.23" "10.10.15.31" "10.10.15.32" "10.10.15.33" )
 
-read -sp "ubuntu 계정의 비밀번호를 입력하세요: " PASSWORD
+read -sp "ubuntu account password? : " PASSWORD
 echo ""
 
 for IP in "${SERVERS[@]}"; do
