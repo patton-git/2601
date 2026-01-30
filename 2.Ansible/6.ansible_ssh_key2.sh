208a7e5247ecf503930a83c2eff829ff1c0b3777
patton-git <63215900+patton-git@users.noreply.github.com>
2026-01-30 16:15:50 +0900

    Update 6.ansible_ssh_key2.sh

--- 2.Ansible/6.ansible_ssh_key2.sh
+++ 2.Ansible/6.ansible_ssh_key2.sh
@@ -3,7 +3,7 @@
 if [ ! -f ~/.ssh/ansible_key ]; then ssh-keygen -t rsa -b 4096 -f ~/.ssh/ansible_key -N "" 
 fi
 
-SERVERS=( "10.10.15.10" "10.10.15.11" "10.10.15.12" "10.10.15.21" "10.10.15.22" "10.10.15.23" "10.10.15.31" "10.10.15.32" "10.10.15.33" )
+SERVERS=( "10.10.15.11" "10.10.15.12" "10.10.15.21" "10.10.15.22" "10.10.15.23" "10.10.15.31" "10.10.15.32" "10.10.15.33" )
 
 read -sp "ubuntu account password? : " PASSWORD
 echo ""
@@ -11,3 +11,4 @@ echo ""
 for IP in "${SERVERS[@]}"; do
     sshpass -p "$PASSWORD" ssh-copy-id -i ~/.ssh/ansible_key.pub -o StrictHostKeyChecking=no ubuntu@$IP
 done
+
